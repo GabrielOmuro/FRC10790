1360b53aaba52bc49d3612a608d62bdc55b48eba
unknown <gabriel_omuro@estudante.sesisenai.org.br>
2025-05-31 17:31:43 -0300

    altUpdate

--- src/components/navbarPQNI.jsx
+++ src/components/navbarPQNI.jsx
@@ -5,7 +5,7 @@ function NavbarPQNI() {
   return (
     <>
       <div className="navbarPQNI">
-        <img className="logoPQNI" src="public/images/logo.jpg" alt="imagem não encontrada" />
+        <img className="logoPQNI" src="../public/images/logo.jpg" alt="imagem não encontrada" />
         <Link className="LinkPQNI" to="/">
           Inicio
         </Link>
