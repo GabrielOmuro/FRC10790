e14ea3882a9c790b883c2b15433fa5fb931aa9ce
unknown <gabriel_omuro@estudante.sesisenai.org.br>
2025-06-02 13:39:58 -0300

    lastCommit

--- src/pages/arasunuap1.jsx
+++ src/pages/arasunuap1.jsx
@@ -1,7 +1,12 @@
 import "./arasunuap1.css";
 import Rodape from "../components/rodape";
 import { Link } from "react-router-dom";
-import logo from "../../public/images/logo.jpg"
+import logo from "../../public/images/logo.jpg";
+import arasunuap1 from "../../public/images/arasunuap1.webp";
+import materials from "../../public/images/materials.jpg";
+import fluxogramaSC from "../../public/images/fluxogramaSC.jpg";
+import shooterAlgorithm from "../../public/images/shooterAlgorithm.png";
+import algorithmWH from "../../public/images/algorithmWH.png"
 
 function Arasunuap1() {
   return (
@@ -25,12 +30,61 @@ function Arasunuap1() {
         </div>
       </header>
       <div className="divContent">
-        <h1>Bem-vindo à página que fala sobre o nosso robô!</h1>
+        <h1>Arasunu AP1</h1>
         <p>
-          Infelizmente esta página ainda não está pronta! Nossos Desenvolvedores
-          e Engenheiros estão trabalhando o mais rápido possível, mas ainda não
-          está pronta!
+          Esse é o nosso robô de competição, o Arasunu AP1. Seu nome vem do
+          Guarani que Significa "Trovão Auto Propelido 1".
         </p>
+        <img
+          className="arasunuap1Image"
+          src={arasunuap1}
+          alt="imagem não encontrada"
+        />
+      </div>
+      <div id="robotProcess" className="robotProcess">
+        <h1>Processo de Criação:</h1>
+        <p>
+          Para chegar a este modelo do robô, foram lidos atentamente os
+          documentos relativos à organização da partida, regras de pontuação,
+          tamanho, e etc. Após isso, visando a melhor estratégia a ser utilizada
+          para procurar maximizar a pontuação da aliança, foram decididos os
+          componentes básicos mais necessários no robô. como um atirador, um
+          gancho para a chain e o skid, entre outros. Então, tomando como
+          inspiração o robô “ruckus”, da equipe FRC118, foram instituídos a base
+          e shooter do robô, e em seguida criados os sistemas do braço
+          articulado e garras para coleta da kapii. sempre buscando que o robô
+          fosse o mais autônomo e flexível possível.
+        </p>
+      </div>
+      <div id="materials" className="materials">
+        <h1>Os materiais utilizados no robô estão na tabela abaixo:</h1>
+        <img src={materials} alt="" />
+      </div>
+      <div id="algorithms" className="algorithmsSCS">
+        <h1>Algoritmos da Smoke Cloud:</h1>
+        <p>
+          No primeiro nível, como haverá fumaça na arena, consequentemente a
+          visibilidade estará extremamente baixa, portanto opatmos por deixar o
+          robô no modo autônomo. O fluxograma da Smoke Cloud está inserido
+          abaixo:
+        </p>
+        <img src={fluxogramaSC} alt="" />
+      </div>
+      <div className="algorithmsShooter">
+        <h1>Algoritmo do Shooter:</h1>
+        <p>
+          O shooter é a funcionalidade responsável por lançar as Water Drops
+          (WDs) dentro do Tank.
+        </p>
+        <img src={shooterAlgorithm} alt="" />
+      </div>
+      <div className="algorithmWH">
+        <h1>Algoritmo das rodas e das Garras: </h1>
+        <p>
+          O shooter é a funcionalidade responsável por lançar as Water Drops
+          (WDs) dentro do Tank.
+        </p>
+        <img className="algorithmWHimg" src={algorithmWH}></img>
       </div>
       <div className="containerEspacamentoArasunuap1"></div>
     </>
--- src/pages/history.jsx
+++ src/pages/history.jsx
@@ -10,15 +10,25 @@ function History() {
       <div className="historyDiv">
         <h1>Nossa História</h1>
         <p>
-          Esta é uma equipe fictícia criada para o processo seletivo, mas, as
-          pessoas que estão suando por ela são reais. Todos os membros desta
-          equipe tiveram uma contribuição significativa para o progresso
-          coletivo. Algumas pessoas se esforçaram em marketing, decidindo cores,
-          nome da equipe, logo; outras pessoas deram tudo de si em prêmios
-          pensando em patrocínios e causas sociais para concorrer ao Impact
-          Award da FRC5800; Além dessas pessoas, tiveram aqueles que criaram um
-          robô, como os alunos da área de engenharia e os alunos de programação
-          que deram ao robô uma vida. Todos tiveram uma participação forte,
+          FRC 10790 foi fundada em 2023 por cinco estudantes, com o apoio de
+          Sérgio de Oliveira Manga, engenheiro formado em Engenharia de Controle
+          e Automação e professor do IFBA (Instituto Federal da Bahia). Com sede
+          no Instituto Federal de Salvador, a equipe nasceu do sonho de tornar a
+          robótica mais acessível e de incentivar os jovens a se envolverem com
+          ciência e tecnologia. Com o apoio adicional da AIDTEC (Associação
+          Internacional para o Desenvolvimento Tecnológico), uma organização não
+          governamental criada para fomentar e promover a inovação em ciência,
+          tecnologia e educação, começamos a desenvolver projetos sociais e
+          educacionais como "Asas que Motivam" e "Conhecer, Sonhar e Voar". Esta
+          é uma equipe fictícia criada para o processo seletivo, mas, as pessoas
+          que estão suando por ela são reais. Todos os membros desta equipe
+          tiveram uma contribuição significativa para o progresso coletivo.
+          Algumas pessoas se esforçaram em marketing, decidindo cores, nome da
+          equipe, logo; outras pessoas deram tudo de si em prêmios pensando em
+          patrocínios e causas sociais para concorrer ao Impact Award da
+          FRC5800; Além dessas pessoas, tiveram aqueles que criaram um robô,
+          como os alunos da área de engenharia e os alunos de programação que
+          deram ao robô uma vida. Todos tiveram uma participação forte,
           abrangendo todas as áreas.
         </p>
       </div>
